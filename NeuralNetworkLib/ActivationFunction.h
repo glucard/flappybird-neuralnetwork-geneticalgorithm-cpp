c5ccaf342235654e776609980ea77d22eecdb140
glucas11 <glucas11@hotmail.com>
2021-04-17 21:58:56 -0400

    Class NeuralNetwork added.

---
+++ NeuralNetworkLib/ActivationFunction.h
@@ -0,0 +1,11 @@
+#ifndef _ACTIVATION_FUNCTION_H_
+#define _ACTIVATION_FUNCTION_H_
+
+#include "Array2DLib/Array2D.h"
+
+namespace ActivationFunction { // arrumar retorno.
+	void ReLU(void* x);
+	void sigmoid(void* x);
+}
+
+#endif // !_ACTIVATION_FUNCTION_H_
